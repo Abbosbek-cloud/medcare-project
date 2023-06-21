6b8de0b3823073c24cca2d8301fffc8362cc00ed
Abbosbek <abek01sulaymonov@gmail.com>
2023-06-21 11:57:35 +0500

    feature: doctorSlice created | fix: fonts fixed

---
+++ src/store/doctor.slice.js
@@ -0,0 +1,37 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getAllDoctors } from "./requests";
+
+const initialState = {
+  isLoading: false,
+  doctors: null,
+  error: "",
+};
+
+export const GetAllDoctorsThunk = createAsyncThunk(
+  // mana shu string ichidagi qiymatlar unique (takrorlanmas) bo'lishi kerak
+  "doctors/getAll",
+  getAllDoctors
+);
+
+export const doctorSlice = createSlice({
+  name: "doctor",
+  reducers: {},
+  initialState,
+  extraReducers: (builder) => {
+    // bitta thunk ichida ikkita phase (pending, fulfilled, rejected) yozilmasligi kerak
+    builder.addCase(GetAllDoctorsThunk.pending, function (state, action) {
+      state.isLoading = true;
+    });
+    builder.addCase(GetAllDoctorsThunk.fulfilled, function (state, action) {
+      state.isLoading = false;
+      state.doctors = action.payload;
+    });
+    builder.addCase(GetAllDoctorsThunk.rejected, function (state, action) {
+      state.isLoading = false;
+    });
+  },
+});
+
+export const {} = doctorSlice.actions;
+
+export default doctorSlice.reducer;
--- src/store/root.js
+++ src/store/root.js
@@ -1,8 +1,11 @@
 import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import doctorSlice from "./doctor.slice";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  doctorSlice: doctorSlice,
+});
 
 export default persistReducer(
   {
