3e15958e258282c8aad161787eb43bc92b76e56a
Soliha <solyatursuntasheva@gmail.com>
2023-06-25 22:11:46 +0500

    Add: MenuDrawer content to that

--- src/components/drawers/MenuDrawer.js
+++ src/components/drawers/MenuDrawer.js
@@ -1,29 +1,100 @@
 import React from "react";
-import { Drawer, Box, Stack, Typography } from "@mui/material";
+import { Drawer, Box, Stack, Typography, Container } from "@mui/material";
 import { useRouter } from "next/router";
+import CancelIcon from "components/icons/CancelIcon";
+import NavItem from "components/navItem";
+import { IconBox } from "components/layout/main";
+
+import FaceBookIcon from "components/icons/FaceBookIcon";
+import InstagramIcon from "components/icons/InstagramIcon";
+import TelegramIcon from "components/icons/TelegramIcon";
 
 const MenuDrawer = (props) => {
   const router = useRouter();
   const list = (anchor) => (
     <Box
-      sx={{ width: 300, p: 2 }}
+      sx={{ width: 300, p: 2, bgcolor: "#F9F9F9", height: "100%" }}
       role="presentation"
-      onClick={() => props.toggleDrawer(false)}
       onKeyDown={() => props.toggleDrawer(false)}
     >
       {/* drawer components come here */}
-      <Stack
-        direction="row"
-        justifyContent="start"
-        alignItems="center"
-        gap={1}
-        sx={{ cursor: "pointer" }}
-        onClick={() => router.push("/")}
-      >
-        <Box component="img" src="/svg.svg" />
-        <Typography variant="logoPrint" fontWeight="500" color="success.main">
-          MedCare
-        </Typography>
+      <Stack px={0.1} direction="column" gap={10}>
+        <Stack
+          px={1}
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Box
+            display={"flex"}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            gap={1}
+            sx={{ cursor: "pointer" }}
+            onClick={() => router.push("/")}
+          >
+            <Box component="img" src="/svg.svg" />
+            <Typography
+              variant="logoPrint"
+              fontWeight="300"
+              fontSize={22}
+              color="success.main"
+            >
+              MedCare
+            </Typography>
+          </Box>
+          <CancelIcon onClick={() => props.toggleDrawer(false)} />
+        </Stack>
+        <Stack direction="column" alignItems={"center"} gap={3}>
+          <NavItem
+            link="/aboutus"
+            name="Biz haqimizda"
+            isActive={router.pathname.startsWith("/aboutus")}
+            isLast={true}
+          />
+          <NavItem
+            link="/doctors"
+            name="Shifokorlar"
+            isActive={router.pathname.startsWith("/doctors")}
+            isLast={true}
+          />
+          <NavItem
+            link="/clinics"
+            name="Klinikalar"
+            isActive={router.pathname.startsWith("/clinics")}
+            isLast={true}
+          />
+          <NavItem
+            link="/pills"
+            name="Dori-darmonlar"
+            isActive={router.pathname.startsWith("/pills")}
+            isLast={true}
+          />
+          <NavItem
+            link="/topics"
+            name="Maqolalar"
+            isActive={router.pathname.startsWith("/topics")}
+            isLast={true}
+          />
+          <NavItem
+            link="/contacts"
+            name="Kontaktlar"
+            isActive={router.pathname.startsWith("/contacts")}
+            isLast={true}
+          />
+        </Stack>
+        <Stack direction="row" justifyContent={"center"} gap={2}>
+          <IconBox>
+            <FaceBookIcon />
+          </IconBox>
+          <IconBox>
+            <InstagramIcon />
+          </IconBox>
+          <IconBox>
+            <TelegramIcon />
+          </IconBox>
+        </Stack>
       </Stack>
     </Box>
   );
