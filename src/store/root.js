3779156e051fd3f988e45bea45d34de8ca6d3c5e
Abbosbek <abek01sulaymonov@gmail.com>
2023-06-26 00:22:02 +0500

    feature: search logic created

--- src/store/root.js
+++ src/store/root.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import doctorSlice from "./doctor.slice";
+import searchSlice from "./search.slice";
 
 const rootReducer = combineReducers({
   doctorSlice: doctorSlice,
+  search: searchSlice,
 });
 
 export default persistReducer(
---
+++ src/store/search.slice.js
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  sick: [],
+  selected: [],
+  isLoading: false,
+};
+
+export const searchSlice = createSlice({
+  name: "search",
+  initialState,
+  reducers: {
+    setIllness(state, action) {
+      state.sick = action.payload;
+    },
+    toggleIllness(state, action) {
+      const isExist = state.sick.find((item) => item.id === action.payload.id);
+      if (isExist) {
+        let filtered = state.sick.filter(
+          (item) => item.id !== action.payload.id
+        );
+        state.sick = filtered;
+      } else {
+        state.sick = [...state.sick, action.payload];
+      }
+    },
+    toggleSelected(state, action) {
+      const isExist = state.selected.find(
+        (item) => item.id === action.payload.id
+      );
+      if (isExist) {
+        let filtered = state.selected.filter(
+          (item) => item.id !== action.payload.id
+        );
+        state.selected = filtered;
+      } else {
+        state.selected = [...state.selected, action.payload];
+      }
+    },
+  },
+});
+
+export const { setIllness, toggleIllness, toggleSelected } =
+  searchSlice.actions;
+
+export default searchSlice.reducer;
